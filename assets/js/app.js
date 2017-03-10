cdbfacf9f9f00683be697c2b0b4bc34b7ee91ade
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-10 17:56:25 -0500

    Validando

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,55 @@
-var email = document.getElementById('input-Email').value;
-var password = document.getElementById('input-Password').value;
-var campoEmail = document.getElementById('campoEmail');
-var campoPassword = document.getElementById('campoPassword');
+// Vamo a crear variables globales
+var usuariosTotales= [];
+var email= document.getElementById("input-Email").value;
+var password= document.getElementById("input-Password").value;
+var campoEmail = document.getElementsByTagName('span');
+var campoPassword = document.getElementsByTagName('span');
+/*
+//Vamo a poner reglas :sad_parrot:
+var button = document.getElementById("login");
+function funciones(){
+  if (button.addEventListener) {
+    button.addEventListener("click", myFunction);
+  } else if (button.attachEvent) {
+    button.attachEvent("onclick", myFunction);
+  }
+}
+*/
 
-function validarLoguin(email,password){
+//Funcion login
+window.addEventListener('load',function() {
+  var loginButton = document.getElementById('login');
+  loginButton.addEventListener('click',function(){
+    var emailInput = document.getElementById('input-Email');
+    var passwordInput = document.getElementById('input-Password');
+  });
+});
+
+
+function validarLogin(email,password){
   if(!/\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{3,})\z/.test(email)){
-    campoEmail.innerHTML = "La dirección de correo electrónico no es válido";
+    document.getElementById("campoEmail").innerHTML = "La dirección de correo electrónico no es válida";
   } else {
-    alert ('Esta direccion es correcta');
+  // vamos a guardarlo;
   }
   if(!/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/.test(password)){
-    alert ('La contraseña del correo ' + password + ' no es valido');
+    document.getElementById("campoPassword").innerHTML = "La contraseña no es válida";;
   } else {
-    alert ('Esta contraseña es correcta');
+  // vamos a guardarlo;
+  }
+}
+
+//Vamo a guardar los usuarios :fast_parrot:
+function guardarUsuarios() {
+  var usuario = new ingreso(email,password);
+  usuariosTotales.push(usuario);
+}
+
+//Vamo a prohibir usuarios :thinking_face:
+function registro(){
+  var registroUsuarios = "";
+  var cantidadUsuarios = 6;
+  for (var i= 0; i<=cantidadUsuarios; i++){
+    registroUsuarios = registroUsuarios + usuariosTotales[i];
   }
 }
